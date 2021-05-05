4dfe206562a746992b011019a8a2ba92016195fe
Chen-Hai Teng <chenhai.teng@positivegrid.com>
2021-05-05 14:27:19 +0800

    Add HandAiguille to easy create hand aiguille. 1. Link time data to hand aiguille to change its rotation effect 2. Allow developer to create its own hand aiguille style by simply combine different views. 3. Add factory function to create apple watch style hand aiguille.

---
+++ Sources/Rings/HandAiguille.swift
@@ -0,0 +1,166 @@
+//
+//  HandAiguille.swift
+//  
+//
+//  Created by Chen Hai Teng on 5/4/21.
+//
+
+import SwiftUI
+import Foundation
+
+public enum TimeUnit {
+    case hour, minute, second
+}
+
+extension TimeUnit {
+    public typealias RawValue = Calendar.Component
+    public init?(rawValue: RawValue) {
+        switch rawValue {
+        case .hour: self = Self.hour
+        case .minute: self = Self.minute
+        case .second: self = Self.second
+        default: return nil
+        }
+    }
+}
+
+public struct HandAiguille<Content: View> : View {
+    
+    @Binding private var time: Double
+    @State private var angle: Angle = Angle()
+    
+    private let content: () -> Content
+    private var handSize: CGSize
+    private var offset: CGFloat
+    private var timeUnit: TimeUnit
+    
+    private var showBlueprint: Bool = false
+    
+    public init(size: CGSize, offset: CGFloat, time: Binding<Double> = .constant(0), unit: TimeUnit = .second, @ViewBuilder content: @escaping () -> Content) {
+        self.handSize = size
+        self.offset = offset
+        _time = time
+        timeUnit = unit
+        self.content = content
+    }
+    
+    public var body: some View {
+        GeometryReader { geo in
+            ZStack {
+                let yoffset:CGFloat = handSize.height/2.0 - offset
+                if(showBlueprint) {
+                    Circle().stroke().frame(width:geo.size.width, height: geo.size.height)
+                }
+                Path { p in
+                    p.move(to: CGPoint(x: 0.0, y: geo.size.height/2.0))
+                    p.addLine(to: CGPoint(x: geo.size.width, y: geo.size.height/2.0))
+                    p.move(to: CGPoint(x: geo.size.width/2, y: 0.0))
+                    p.addLine(to: CGPoint(x: geo.size.width/2, y: geo.size.height))
+                }.if(showBlueprint) { p in
+                    p.stroke(Color.blue)
+                }
+                content().frame(width: handSize.width, height: handSize.height, alignment: .center).offset(y: -yoffset).rotationEffect(angleOfTime(time))
+            }
+        }
+    }
+    
+    private func angleOfTime(_ time: Double) -> Angle {
+        switch timeUnit {
+        case .hour:
+            let t = time.truncatingRemainder(dividingBy: 12.0)
+            return Angle(degrees: t * 30.0)
+        case .minute, .second:
+            let t =
+                time.truncatingRemainder(dividingBy: 60.0)
+            return Angle(degrees: t * 6.0)
+        }
+    }
+}
+
+extension HandAiguille {
+    func setProperty(_ setBlock: (_ handAiguille: inout Self) -> Void) -> Self {
+        let result = _setProperty(content: self) { (tmp :inout Self) in
+            setBlock(&tmp)
+            return tmp
+        }
+        return result
+    }
+    
+    func blueprint(_ isOn:Bool = true) -> Self {
+        setProperty { tmp in
+            tmp.showBlueprint = isOn
+        }
+    }
+}
+
+public struct HandFactory {
+    private let rectRatio: CGFloat = 0.2
+    public func makeWatchStyleHand(size: CGSize = CGSize(width: 4.0, height: 60.0), timeProvider: Binding<Double>, unit: TimeUnit = .second) -> some View {
+        HandAiguille(size: size, offset: 1.5, time: timeProvider, unit: unit) {
+            VStack(spacing: 0) {
+                Capsule().stroke().frame(width: size.width)
+                Rectangle().frame(width: 2, height: 10, alignment: .center)
+                Circle().stroke().frame(width: 3, height: 3, alignment: .center)
+            }
+        }
+    }
+}
+
+struct AppleStyleHandPreview: View {
+    @State var emulateTime: Double = 0.0
+    @State var showBlueprint: Bool = false
+    @State var offset: CGFloat = 1.5
+    @State var unit: TimeUnit = .second
+    
+    var body: some View {
+        VStack {
+            HStack {
+                ZStack {
+                    HandAiguille(size: CGSize(width: 4.0, height: 50.0), offset: offset, time: $emulateTime, unit: unit) {
+                        GeometryReader { geo in
+                            VStack(spacing: 0) {
+                                Capsule().stroke()
+                                Rectangle().frame(width: 2, height: 10, alignment: .center)
+                                Circle().stroke().frame(width: 3, height: 3, alignment: .center)
+                            }
+                        }
+                    }.blueprint(showBlueprint).frame(width: 100, height: 100, alignment: .center)
+                }
+                ZStack {
+                    HandFactory().makeWatchStyleHand(size: CGSize(width: 5.0, height: 80.0),timeProvider: $emulateTime, unit: .hour).frame(width: 120, height: 120, alignment: .center)
+                    HandFactory().makeWatchStyleHand(size: CGSize(width: 4.0, height: 60.0),timeProvider: $emulateTime, unit: .minute).frame(width: 120, height: 120, alignment: .center)
+                }
+            }
+            HStack {
+                Spacer(minLength: 10)
+                Text("time:")
+                Slider(value: $emulateTime, in: 0.0...60.0)
+                Text("\(emulateTime)")
+                Spacer(minLength: 10)
+            }
+            HStack {
+                Spacer(minLength: 10)
+                Text("offset:")
+                Slider(value: $offset, in: 0.0...20.0, step: 0.5)
+                Text("\(offset)")
+                Spacer(minLength: 10)
+            }
+            HStack {
+                Spacer(minLength: 10)
+                Picker("Time Unit", selection: $unit) {
+                    Text("hour").tag(TimeUnit.hour)
+                    Text("minute").tag(TimeUnit.minute)
+                    Text("second").tag(TimeUnit.second)
+                }.pickerStyle(SegmentedPickerStyle())
+                Spacer(minLength: 10)
+            }
+            Toggle("show blueprint", isOn: $showBlueprint)
+        }
+    }
+}
+
+struct HandAiguille_Previews: PreviewProvider {
+    static var previews: some View {
+        AppleStyleHandPreview().frame(width: 350, height: 350, alignment: .center)
+    }
+}
