42dd50d66822f160fab8125d61545001aeeed010
Chen Hai Teng <chenhaiteng@gmail.com>
2021-05-17 20:31:06 +0800

    1. Fix blur range bug 2. Update each text frame depends on font size. 3. Re organize preview to show better result.

--- Sources/Rings/SphericText.swift
+++ Sources/Rings/SphericText.swift
@@ -23,7 +23,8 @@ struct SphericText<T:BinaryFloatingPoint>: View {
     private var frontMostRange = -10...10
     private var perspective: CGFloat = 0.0
     private var anchorZ: CGFloat = 100.0
-    @State private var estHight: CGFloat = 0.0
+    @State private var estHeight: CGFloat = 30.0
+    @State private var estWidth: CGFloat = 30.0
     
     init(words: [String], word_spacing: T = 30.0 , font: Font? = nil, word_color: Color = Color.white, word_background: Color = Color.clear, hide_opposite: Bool = false, degree_offset: Binding<T> = .constant(0)) {
         stringTable = words.enumerated().map { (i, e) in
@@ -70,9 +71,9 @@ struct SphericText<T:BinaryFloatingPoint>: View {
                     let deg = Double(i)*wordSpacing + Double(offsetDegree)
                     let _ = print("degrees: \(deg)")
                     let normalizedD = Int(deg)%360
-                    let shouldBlur = blurMinors ? (frontMostRange ~= normalizedD) : false
+                    let shouldBlur = blurMinors ? !(frontMostRange ~= abs(normalizedD)) : false
                     let shouldHide = hideOpposite ? (oppositeRange ~= abs(normalizedD)) : false
-                    Text(e).frame(width: w/2, height: estHight, alignment: .center)
+                    Text(e).frame(width: estWidth*CGFloat(e.count), height: 50, alignment: Alignment(horizontal: .center, vertical: .center))
                         .font(font)
                         .foregroundColor(wordColor)
                         .background(wordBackground)
@@ -82,12 +83,15 @@ struct SphericText<T:BinaryFloatingPoint>: View {
                             anchor: .center,
                             anchorZ: anchorZ,
                             perspective: perspective
-                        ).opacity(shouldHide ? 0.0 : 1.0).blur(radius: shouldBlur ? 0.0 : 1.0)
-                }
+                        ).opacity(shouldHide ? 0.0 : (shouldBlur ? 0.5 : 1.0))
+                        .blur(radius: (shouldBlur ? 1.0 : 0.0))
+                }.frame(width: w, height: estHeight, alignment: .center)
 
-            }.frame(width: frameL.size.width, height: estHight).onPreferenceChange(ViewSizeKey.self) { sizes in
-                estHight = (sizes.reduce(0, {$0 + $1.height}))/CGFloat(sizes.count)*CGFloat(1.1)
+            }.frame(width: frameL.size.width, height: frameL.size.height).onPreferenceChange(ViewSizeKey.self) { sizes in
+                estHeight = (sizes.reduce(0, {$0 + $1.height}))/CGFloat(sizes.count)*CGFloat(1.1)
+                estWidth = (sizes.reduce(0, {$0 + $1.width}))/CGFloat(sizes.count)
             }
+
         }
     }
 }
@@ -146,6 +150,12 @@ extension SphericText : Adjustable {
             tmp.anchorZ = value
         }
     }
+    
+    public func blurMinors(_ isBlur: Bool) -> Self {
+        setProperty { tmp in
+            tmp.blurMinors = isBlur
+        }
+    }
 }
 
 struct SphericTextDemo: View {
@@ -156,6 +166,11 @@ struct SphericTextDemo: View {
     @State var characters = "ABCDE"
     @State var wordsInput = "Test\n100"
     @State var words = ["Test", "100"]
+    @State var blurMinors: Bool = false
+    @State var hideOpposite: Bool = false
+    @State var textColor: Color = .white
+    @State var backgroundColor: Color = .clear
+    @State var wordSpacing: Double = 100.0
     
     var body: some View {
         let wordsInputBinding = Binding<String>(get: {
@@ -168,24 +183,68 @@ struct SphericTextDemo: View {
         })
         GeometryReader { geo in
             VStack {
-                Group {
-                    ZStack {
-                        SphericText(characters, $rotateDeg).hideOpposite(true).rangeOfOpposite(in: 145...210)
-                            .radius(radius).perspective(perspective).frame(width: geo.size.width, height: 50)
+                HStack {
+                    let width = geo.size.width/2
+                    VStack {
+                        ZStack {
+                            SphericText(characters, $rotateDeg).rangeOfOpposite(in: 145...210)
+                                .radius(radius)
+                                .perspective(perspective)
+                                .blurMinors(blurMinors)
+                                .hideOpposite(hideOpposite)
+                                .frame(width: width)
+                        }
+                        HStack {
+                            Spacer()
+                            TextField("Input Spheric Characters", text: $characters).textFieldStyle(RoundedBorderTextFieldStyle())
+                            Spacer()
+                        }
+                        Toggle("Blur Minor", isOn: $blurMinors)
+                        Toggle("Hide Opposite", isOn: $hideOpposite)
                     }
-                    HStack {
-                        Spacer()
-                        TextField("Input Spheric Characters", text: $characters).textFieldStyle(RoundedBorderTextFieldStyle())
-                        Spacer()
+                    Divider().background(Color.white)
+                    VStack {
+                        SphericText(words: words, degree_offset: $rotateDeg).wordSpacing(wordSpacing).font( .system(size: 32.0)).wordColor(textColor).wordBackground(backgroundColor).hideOpposite(false).perspective(perspective).radius(radius).frame(width: width)
+                        if #available(macOS 11.0, iOS 14.0, macCatalyst 14.0, *) {
+                            TextEditor(text: wordsInputBinding).textFieldStyle(RoundedBorderTextFieldStyle()).frame(width: 100, height: 80, alignment: .center)
+                        } else {
+                            // Fallback on earlier versions
+                            Text(words.reduce("", { result, word in
+                                result + word + ","
+                            }))
+                        }
+                        HStack {
+                            Spacer()
+                            Text("text:")
+                            Picker("", selection: $textColor) {
+                                Text("White").tag(Color.white)
+                                Text("Red").tag(Color.red)
+                                Text("Blue").tag(Color.blue)
+                                Text("Green").tag(Color.green)
+                            }.colorPicker($textColor)
+                            Spacer()
+                            Text("background:")
+                            Picker("", selection: $backgroundColor) {
+                                Text("Clear").tag(Color.clear)
+                                Text("White").tag(Color.white)
+                                Text("Red").tag(Color.red)
+                                Text("Blue").tag(Color.blue)
+                                Text("Green").tag(Color.green)
+                            }.colorPicker($backgroundColor)
+                            Spacer()
+                        }
+                        HStack {
+                            Spacer(minLength: 20)
+                            Button(action: {
+                                wordSpacing = 100.0
+                            }, label: {
+                                Text("word spacing")
+                            })
+                            Slider(value: $wordSpacing, in: 50...200, step: 5.0)
+                            Text("\(wordSpacing, specifier: "%.2f")").foregroundColor(.white)
+                            Spacer(minLength: 20)
+                        }
                     }
-                }
-                Divider().background(Color.white)
-                SphericText(words: words, degree_offset: $rotateDeg).wordSpacing(100.0).font( .system(size: 32.0)).wordColor(.red).hideOpposite(false).perspective(perspective).radius(radius).frame(width: geo.size.width)
-                Spacer(minLength: 50.0)
-                if #available(macOS 11.0, *) {
-                    TextEditor(text: wordsInputBinding).textFieldStyle(RoundedBorderTextFieldStyle()).frame(width: 100, height: 100, alignment: .center)
-                } else {
-                    // Fallback on earlier versions
                 }
                 Divider().background(Color.white)
                 Group {
@@ -194,7 +253,7 @@ struct SphericTextDemo: View {
                         Button(action: {
                             rotateDeg = 0.0
                         }, label: {
-                            Text("Rotate").padding(5).overlay(RoundedRectangle(cornerRadius: 10.0).stroke())
+                            Text("Rotate")
                         })
                         Slider(value: $rotateDeg, in:  -360.0...360.0)
                         Text("\(rotateDeg, specifier: "%.2f")").foregroundColor(.white)
@@ -205,7 +264,7 @@ struct SphericTextDemo: View {
                         Button(action: {
                             radius = 40.0
                         }, label: {
-                            Text("Radius").padding(5).overlay(RoundedRectangle(cornerRadius: 10.0).stroke())
+                            Text("Radius")
                         })
                         Slider(value: $radius, in: 0...500)
                         Text("\(radius, specifier: "%.2f")").foregroundColor(.white)
@@ -215,14 +274,14 @@ struct SphericTextDemo: View {
                         Button(action: {
                             perspective = 0.0
                         }, label: {
-                            Text("Perspective").padding(5).overlay(RoundedRectangle(cornerRadius: 10.0).stroke())
+                            Text("Perspective")
                         })
                         Slider(value: $perspective, in: -1...1)
                         Text("\(perspective, specifier: "%.2f")").foregroundColor(.white)
-                        Spacer(minLength: 0)
+                        Spacer(minLength: 20)
                     }
                 }
-                Spacer(minLength: 40)
+                Spacer(minLength: 10)
             }.background(Color.black)
         }
     }
