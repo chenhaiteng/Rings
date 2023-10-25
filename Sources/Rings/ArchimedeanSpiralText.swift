531c28c35e113636f9131e870f5c62121ff0bb4e
Chen-Hai Teng <chenhaiteng@gmail.com>
2023-10-25 16:16:03 +0800

    support preview on different platform

--- Sources/Rings/ArchimedeanSpiralText.swift
+++ Sources/Rings/ArchimedeanSpiralText.swift
@@ -162,7 +162,8 @@ extension Picker {
     }
 }
 
-struct ArchimedeanSpiralTextDemo : View {
+#if !os(tvOS)
+struct ArchimedeanSpiralText_Preview_macOS : View {
     private let demoText = "1234567890abcdefgABCDEFG♩♪♫♬"
     @State var radiusSpacing: Double = 20.0
     @State var innerR: Double = 25.0
@@ -224,8 +225,109 @@ struct ArchimedeanSpiralTextDemo : View {
     }
 }
 
+struct ArchimedeanSpiralText_Preview_iOS : View {
+    private let demoText = "1234567890abcdefgABCDEFG♩♪♫♬"
+    @State var radiusSpacing: Double = 20.0
+    @State var innerR: Double = 25.0
+    @State var gap: Double = 25.0
+    @State var textLength: Double = 15.0
+    @State var direction: TextDirection = TextDirection.Top
+    @State var color: Color = .red
+    @State var font: Font = .system(.body)
+    
+    public var body: some View {
+        VStack {
+            let enabled = String(demoText.prefix(Int(textLength)))
+            let disabled = String(demoText.suffix(demoText.count - Int(textLength)))
+            ArchimedeanSpiralText(text: enabled)
+                .gap(gap)
+                .innerRadius(innerR)
+                .radiusSpacing(radiusSpacing)
+                .textDirection(direction)
+                .textColor(color)
+                .font(font)
+            Divider()
+            VStack(alignment: .leading, spacing: 0.0) {
+                HStack {
+                    Text(enabled).foregroundColor(color) + Text(disabled).foregroundColor(.gray)
+                }
+                Slider(value: $textLength, in: 1.0...Double(demoText.count), step: 1.0)
+            }
+            ScrollView {
+                VStack(alignment: .leading, spacing: 0.0) {
+                    Text("direction forward to center")
+                    Picker("", selection: $direction) {
+                        Text("top").tag(TextDirection.Top)
+                        Text("bottom").tag(TextDirection.Bottom)
+                        Text("right").tag(TextDirection.Right)
+                        Text("left").tag(TextDirection.Left)
+                    }.segmented()
+                    Divider().padding()
+                    HStack {
+                        Text("text color")
+                        Picker("", selection: $color) {
+                            Text("White").tag(Color.white)
+                            Text("Red").tag(Color.red)
+                            Text("Blue").tag(Color.blue)
+                            Text("Green").tag(Color.green)
+                        }.colorPicker($color)
+                    }
+                    Divider().padding()
+                    Text("font")
+                    Picker("Font", selection: $font) {
+                        Text(".body").tag(Font.system(.body))
+                        Text(".caption").tag(Font.system(.caption))
+                        Text("Zapfino(10)").tag(Font.custom("Zapfino", size: 10.0))
+                    }.segmented()
+                    Divider().padding()
+                    VStack(alignment: .leading, spacing: 0.0) {
+                        Text("Inner Radius: \(innerR)")
+                        Slider(value: $innerR, in: 0.0...30.0, step: 5.0)
+                        Text("Radius Spacing: \(radiusSpacing)")
+                        Slider(value: $radiusSpacing, in: 10.0...80.0, step: 5.0)
+                        Text("Gap:\(gap)")
+                        Slider(value: $gap, in: 10.0...40.0, step: 3.0)
+                    }
+                }
+            }
+        }.padding(EdgeInsets(top: 0.0, leading: 10.0, bottom: 0.0, trailing: 10.0))
+    }
+}
+#endif
+
+@available(tvOS, introduced: 6.0)
+struct ArchimedeanSpiralText_Preview_tvOS : View {
+    private let demoText = "1234567890abcdefgABCDEFG♩♪♫♬"
+    @State var radiusSpacing: Double = 100.0
+    @State var innerR: Double = 50.0
+    @State var gap: Double = 80.0
+    @State var textLength: Double = 30.0
+    @State var direction: TextDirection = TextDirection.Top
+    @State var color: Color = .red
+    @State var font: Font = .system(.title)
+    
+    public var body: some View {
+        VStack {
+            ArchimedeanSpiralText(text: demoText)
+                .gap(gap)
+                .innerRadius(innerR)
+                .radiusSpacing(radiusSpacing)
+                .textDirection(direction)
+                .textColor(color)
+                .font(font)
+        }
+    }
+}
+
 struct ArchimedeanSpiralText_Previews: PreviewProvider {
     public static var previews: some View {
-        ArchimedeanSpiralTextDemo()
+#if os(tvOS)
+        ArchimedeanSpiralText_Preview_tvOS()
+#elseif os(macOS)
+        ArchimedeanSpiralText_Preview_macOS()
+#elseif os(iOS)
+        ArchimedeanSpiralText_Preview_iOS()
+#elseif os(watchOS)
+#endif
     }
 }
