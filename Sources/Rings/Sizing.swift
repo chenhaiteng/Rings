d0f46cd71b995a79019935b00b2b003c5252e19d
Chen-Hai Teng <chenhaiteng@gmail.com>
2023-10-25 16:26:40 +0800

    update reference to avoid redirect to insecurity website

--- Sources/Rings/Sizing.swift
+++ Sources/Rings/Sizing.swift
@@ -1,11 +1,11 @@
 //
 //  Sizing.swift
 //  
+//  Use preference key to get child view size
 //  Refer to:
 //  1. https://swiftui-lab.com/communicating-with-the-view-tree-part-1/
-//  2. https://git.kabellmunk.dk/prototyping-custom-ui-in-swiftui-talk/custom-ui-prototype-in-swiftui/-/blob/master/Custom%20UI%20Prototype/Prototype/CurvedText.swift
-//  3. https://www.youtube.com/watch?v=1BHHybRnHFE
-//  4. https://www.fivestars.blog/articles/preferencekey-reduce/
+//  2. https://www.youtube.com/watch?v=1BHHybRnHFE
+//  3. https://www.fivestars.blog/articles/preferencekey-reduce/
 //
 //  Created by Chen Hai Teng on 4/22/21.
 //
