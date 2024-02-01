65f6f222e005b9c068092e2b70b4fa45d5c2a806
Chen-Hai Teng <chenhaiteng@gmail.com>
2024-02-01 18:51:48 +0800

    [Bug fix] Image Knob degrees not work

--- Sources/Rings/KnobComponents/Layers/ImageKnobLayer.swift
+++ Sources/Rings/KnobComponents/Layers/ImageKnobLayer.swift
@@ -9,11 +9,22 @@ import SwiftUI
 import Common
 
 public struct ImageKnobLayer : AngularLayer {
+    
     var image: Image
     public var isFixed: Bool = false
     
+    public var offset: CGPoint = .zero
+    public var radius: CGFloat = 0.0
+    
     @Clamping(0.0...0.0) public var degree: Double = 0.0
-    public var degreeRange: ClosedRange<Double> = 0.0...0.0
+    public var degreeRange: ClosedRange<Double> {
+        get {
+            $degree
+        }
+        set {
+            $degree = newValue
+        }
+    }
     
     public var body: some View {
         get {
