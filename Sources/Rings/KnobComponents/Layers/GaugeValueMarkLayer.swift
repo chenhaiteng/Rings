297bb204a1dbfbaf3e94f19b24ce00b08b27227f
Chen-Hai Teng <chenhaiteng@gmail.com>
2024-03-04 15:49:29 +0800

    Simplify statement by replace offset func with operator

--- Sources/Rings/KnobComponents/Layers/GaugeValueMarkLayer.swift
+++ Sources/Rings/KnobComponents/Layers/GaugeValueMarkLayer.swift
@@ -33,7 +33,7 @@ struct GaugeValueMarkLayer<V> : AngularLayer where V : View {
             ZStack {
                 GeometryReader { geo in
                     let polar = CGPolarPoint(radius: radius - inset, angle: CGAngle.degrees(degree))
-                    let point = polar.cgpoint.offset(dx: geo.size.width/2.0 + offset.x, dy: geo.size.height/2.0 + offset.y)
+                    let point = (polar.cgpoint >> geo.localCenter) >> offset
                     markBuilder().frame(width: markLength, height: markLength).rotationEffect(Angle(degrees: degree + 90.0))
                         .offset(x: point.x - markLength/2.0, y: point.y - markLength/2.0)
                 }
