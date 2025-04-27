839c9d23376c0e206604fb14fd68f7802970d783
Chen-Hai Teng <chenhaiteng@gmail.com>
2025-04-27 14:51:12 +0800

    upgrade dependencies

--- Package.swift
+++ Package.swift
@@ -18,17 +18,13 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-//        .package(url: "https://github.com/chenhaiteng/CoreGraphicsExtension.git", from: "0.4.0"),
-        .package(url: "https://github.com/chenhaiteng/CoreGraphicsExtension.git", branch: "feature/add-demo-app"),
-//        .package(url: "https://github.com/chenhaiteng/ArchimedeanSpiral.git", from: "1.0.12"),
-        .package(url: "https://github.com/chenhaiteng/ArchimedeanSpiral.git", branch: "feature/update-to-swift-6.1"),
-//        .package(url: "https://github.com/chenhaiteng/GradientBuilder.git", from: "1.0.0"),
-        .package(url: "https://github.com/chenhaiteng/GradientBuilder.git", branch: "feature/update-to-swift-6.1"),
+        .package(url: "https://github.com/chenhaiteng/CoreGraphicsExtension.git", from: "0.5.0"),
+        .package(url: "https://github.com/chenhaiteng/ArchimedeanSpiral.git", from: "1.1.0"),
+        .package(url: "https://github.com/chenhaiteng/GradientBuilder.git", from: "1.2.0"),
         .package(url: "https://github.com/andtie/SequenceBuilder.git", from: "0.0.7"),
         .package(url: "https://github.com/GeorgeElsham/ViewExtractor", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
-//        .package(url: "https://github.com/chenhaiteng/SwiftClamping", from: "1.0.0")
-        .package(url: "https://github.com/chenhaiteng/SwiftClamping", branch: "feature/update-to-swift-6.1")
+        .package(url: "https://github.com/chenhaiteng/SwiftClamping", from: "1.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
